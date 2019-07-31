fea3b3faf58ea3dd7d8f0a25973479d76b10149a
Inaba <sheng13358167963@gmail.com>
2019-07-31 20:59:48 +0800

    login page 1st

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If iView is successfully added to this project, you'll see an
-        <code v-text="'<Button>'"></code>
-        below
-      </p>
-      <Button type="primary">Button</Button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {};
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss" scoped></style>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
-import Vue from "vue";
-import Vuex from "vuex";
+/* jshint esversion: 6 */
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
