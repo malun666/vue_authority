b2d921cb90a92d1c78c68baac3d7a77676c71f1b
Inaba <sheng13358167963@gmail.com>
2019-07-31 15:29:22 +0800

    install iview

---
+++ src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login"></div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
