04592d4d0de1f274e737a8b7e7b1b9253e6a4d7a
Inaba <sheng13358167963@gmail.com>
2019-08-01 15:08:29 +0800

    Login page 2nd

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Login from './views/Login.vue';
+import Home from './views/Home.vue';
 
 Vue.use(Router);
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
     },
     {
       path: '/about',
